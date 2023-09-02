3d862033f0499bf2ca58164a85d1f506e0804cfe
Ra1n <theraingame@mail.ru>
2023-09-02 05:58:32 +0300

    redirect

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,17 +1,22 @@
 'use client';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 import Loading from '@/components/Loading';
 import OrderItems from '@/components/OrderItems';
 
 const Orders = () => {
     const session = useSession();
+    const router = useRouter();
     console.log(session);
 
     if (session.status === 'loading') {
         return <Loading />;
     }
 
+    if (session.status === 'unauthenticated') {
+        router.push('/login');
+    }
     return (
         <div className="mt-5 d-flex flex-column align-items-center">
             <h1 className="mb-1 text-center">Список заявок</h1>
