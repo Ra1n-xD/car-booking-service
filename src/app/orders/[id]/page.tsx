e5c50541c1ae8e1eaae4afe5a54120bd064c70d8
Ra1n <theraingame@mail.ru>
2023-09-03 02:29:06 +0300

    Order page id

--- src/app/orders/[id]/page.tsx
+++ src/app/orders/[id]/page.tsx
@@ -1,26 +1,42 @@
-import { getOrder } from '@/services/getData';
+import Link from 'next/link';
 
 import { FaCheckCircle } from 'react-icons/fa';
 import { format } from 'date-fns';
 
-import Link from 'next/link';
+import { getOrder } from '@/services/getData';
+import { authConfig } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth/next';
+import { redirect } from 'next/navigation';
 
-const OrderPage = async ({ params }: { params: { id: string } }) => {
-    const order = await getOrder(params.id);
-    console.log(order);
+export const getServerSideProps = (context: any) => {
+    console.log(context.query);
+    return {
+        props: {
+            index: context.query.index
+        }
+    };
+};
+
+const OrderPage = async ({ params, searchParams }: any) => {
+    const currentOrder = await getOrder(params.id);
+    const session = await getServerSession(authConfig);
+
+    if (session?.user?.email !== currentOrder.userId) {
+        redirect('/');
+    }
 
     return (
         <div className="mt-5">
-            <div className="mb-3 d-flex justify-content-center align-items-center offset">
+            <div className="mb-2 d-flex justify-content-center align-items-end offset">
                 <FaCheckCircle size={50} color="#0080ff" />
-                <h2>Ваша заявка №{params.id}</h2>
+                <h2>Ваша заявка №{searchParams.index}</h2>
             </div>
 
-            <p className="lead mt-2 text-center ">
-                Автомобиль: {order.auto.brand} {order.auto.model.name}
-            </p>
-            <p className="lead mt-2 text-center ">Статус заявки: {order.status.code}</p>
-            <p className="lead mt-2 text-center">Дата заявки: {format(new Date(order.createDate), 'dd.MM.yyyy')}</p>
+            <div className="lead mt-2 text-center ">
+                Автомобиль: {currentOrder.auto.brand} {currentOrder.auto.model.name}
+            </div>
+            <div className="lead mt-2 text-center ">Статус заявки: {currentOrder.status.code}</div>
+            <div className="lead mt-2 text-center">Дата заявки: {format(new Date(currentOrder.createDate), 'dd.MM.yyyy')}</div>
 
             <div className="mb-4 d-flex offset justify-content-center">
                 <Link href="/orders">
