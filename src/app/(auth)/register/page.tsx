a3abe6873aeffd6779749a391bb11439543bca14
Ra1n <theraingame@mail.ru>
2023-09-15 19:44:48 +0300

    Fix spam requests in registration

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -4,12 +4,19 @@ import { useRouter } from 'next/navigation';
 
 const Register = () => {
     const router = useRouter();
-    const [error, setError] = useState(false);
+    const [error, setError] = useState<string | null>(null);
 
     const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
         e.preventDefault();
 
         const formData = new FormData(e.currentTarget);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        if (!email || !password) {
+            setError('Введите данные');
+            return;
+        }
 
         try {
             const res = await fetch('/api/auth/register', {
@@ -17,17 +24,14 @@ const Register = () => {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({
-                    email: formData.get('email'),
-                    password: formData.get('password')
-                })
+                body: JSON.stringify({ email, password })
             });
 
             if (res.status === 201) {
-                setError(false);
+                setError(null);
                 router.push('/login?success=Account has been created');
             } else {
-                setError(true);
+                setError('Данная почта уже зарегистрирована');
             }
         } catch (err: any) {
             throw new Error(err);
@@ -42,7 +46,7 @@ const Register = () => {
                 <input type="email" name="email" className={`mb-4 form-control ${error ? 'is-invalid' : ''}`} placeholder="Email" />
                 <input type="password" name="password" className={`mb-3 form-control ${error ? 'is-invalid' : ''}`} placeholder="Пароль" />
 
-                {error && <div className="mb-2 text-danger">Данная почта уже зарегистрирована</div>}
+                {error && <div className="mb-2 text-danger">{error}</div>}
 
                 <button type="submit" className="btn btn-primary col-12 mt-2">
                     Зарегистрироваться
